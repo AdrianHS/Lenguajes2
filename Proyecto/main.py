b66a7041bfc93dcf17d90c4547d68d0c16f27354
AdrianHS <ad1190@hotmail.com>
2016-09-13 11:07:53 -0600

    Cambio
    Se hicieron más paginas HTML, clases, funciones,etc

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -2,18 +2,24 @@ from flask import Flask, render_template,request,redirect,url_for
 import mysql.connector
 from Clases.Animal import *
 from Clases.Enfermedad import *
+from Clases.Medicamentos import *
+from Clases.Usuario import *
+from Clases.Prescripcion import *
+from Clases.Dosis import *
 from flask_paginate import Pagination
 
 listaAnimales=[]
 listaEnfermedades=[]
 listaMedicamentos=[]
+listaUsuarios = []
+listaPrescripcion = []
+listaDosis = []
 
-#Hola
+
 def consulta():
-    conn =mysql.connector.connect(user='root',password='1234',host='localhost',database='veterinaria')
+    conn =mysql.connector.connect(user='root',password='12345',host='localhost',database='veterinaria')
     mycursor = conn.cursor()
     mycursor.execute("SELECT * FROM animal")
-    #print("ID     Nombre   edad   descripcion")
 
     for element in mycursor.fetchall():
         animal=Animal()
@@ -23,7 +29,7 @@ def consulta():
 consulta()
 
 def consultaEnfermedad():
-    conn =mysql.connector.connect(user='root',password='1234',host='localhost',database='veterinaria')
+    conn =mysql.connector.connect(user='root',password='12345',host='localhost',database='veterinaria')
     mycursor = conn.cursor()
     mycursor.execute("SELECT * FROM enfermedad")
 
@@ -34,6 +40,53 @@ def consultaEnfermedad():
 
 consultaEnfermedad()
 
+def consultaMedicamentos():
+    conn =mysql.connector.connect(user='root',password='12345',host='localhost',database='veterinaria')
+    mycursor = conn.cursor()
+    mycursor.execute("SELECT * FROM medicamentos")
+
+    for element in mycursor.fetchall():
+        medi=Medicamentos()
+        medi.crear(element[0],element[1],element[2])
+        listaMedicamentos.append(medi)
+
+consultaMedicamentos()
+
+def consultaUsuario():
+    conn =mysql.connector.connect(user='root',password='12345',host='localhost',database='veterinaria')
+    mycursor = conn.cursor()
+    mycursor.execute("SELECT * FROM usuario")
+
+    for element in mycursor.fetchall():
+        user=Usuario()
+        user.crear(element[0],element[1],element[2],element[3],element[4])
+        listaUsuarios.append(user)
+
+consultaUsuario()
+
+def consultaPrescripcion():
+    conn =mysql.connector.connect(user='root',password='12345',host='localhost',database='veterinaria')
+    mycursor = conn.cursor()
+    mycursor.execute("SELECT * FROM prescripcion")
+
+    for element in mycursor.fetchall():
+        pres=Prescripcion()
+        pres.crear(element[0],element[1],element[2],element[3],element[4],element[5])
+        listaPrescripcion.append(pres)
+
+consultaPrescripcion()
+
+def consultaDosis():
+    conn =mysql.connector.connect(user='root',password='12345',host='localhost',database='veterinaria')
+    mycursor = conn.cursor()
+    mycursor.execute("SELECT * FROM dosis")
+
+    for element in mycursor.fetchall():
+        dosis=Dosis()
+        dosis.crear(element[0],element[1],element[2],element[3],element[4],element[5])
+        listaDosis.append(dosis)
+
+consultaDosis()
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -42,9 +95,9 @@ def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            return redirect(url_for('animal'))
+            return redirect(url_for('principal'))
         else:
-            return redirect(url_for('animal'))
+            return redirect(url_for('principal'))
     return render_template('login.html')
 
 #Metodo para hacer paginacion
@@ -68,6 +121,10 @@ def separar(num,pagina,count,lista1):
             lista.append(lista1[x])
         list.append(lista1[count - a:])
         return list[pagina]
+
+@app.route('/principal')
+def principal():
+    return render_template('principal.html')
 
 @app.route('/animal')
 def animal():
@@ -94,13 +151,67 @@ def enfermedad():
     enfermedad = separar(5,page,len(listaEnfermedades),listaEnfermedades)
     pagination = Pagination(page=page, total=len(listaEnfermedades),per_page=5, search=search)
     return render_template('enfermedad.html',
-                           animales=enfermedad,
                            pagination=pagination, listaEnfermedades=enfermedad,
                            )
+
+@app.route('/medicamentos')
+def medicamentos():
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
+
+    page = request.args.get('page', type=int, default=1)
+    medi = separar(5,page,len(listaMedicamentos),listaMedicamentos)
+    pagination = Pagination(page=page, total=len(listaMedicamentos),per_page=5, search=search)
+    return render_template('medicamentos.html',
+                           pagination=pagination, listaMedicamentos=medi,
+                           )
+
+@app.route('/usuario')
+def usuario():
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
+
+    page = request.args.get('page', type=int, default=1)
+    user = separar(5,page,len(listaUsuarios),listaUsuarios)
+    pagination = Pagination(page=page, total=len(listaUsuarios),per_page=5, search=search)
+    return render_template('usuario.html',
+                           pagination=pagination, listaUsuarios=user,
+                           )
+
+@app.route('/prescripcion')
+def prescripcion():
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
+
+    page = request.args.get('page', type=int, default=1)
+    pres = separar(5,page,len(listaPrescripcion),listaPrescripcion)
+    pagination = Pagination(page=page, total=len(listaPrescripcion),per_page=5, search=search)
+    return render_template('prescripcion.html',
+                           pagination=pagination, listaPrescripcion=pres,
+                           )
+
+@app.route('/dosis')
+def dosis():
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
+
+    page = request.args.get('page', type=int, default=1)
+    dos = separar(5,page,len(listaDosis),listaDosis)
+    pagination = Pagination(page=page, total=len(listaDosis),per_page=5, search=search)
+    return render_template('dosis.html',
+                           pagination=pagination, listaDosis=dos,
+                           )
+
 
 #Es el encargado de correr el programa
 if __name__ == "__main__":
     app.run(debug=True,port=8080)
 
-
-  #  dfsghjk.lfytynrtfdvycxbhstr jg
